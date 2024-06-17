99c28411e9d47c02a106f7157c7af35205b2cc17
smhrd <smhrd@192.168.219.59>
2024-06-17 18:53:19 +0900

    곽세진 - SearchElement 수정 (lombok 사용 불가로 생성자, getter,setter 직접 입력)

--- withpapa/src/main/java/com/smhrd/withpapa/model/SearchElement.java
+++ withpapa/src/main/java/com/smhrd/withpapa/model/SearchElement.java
@@ -16,5 +16,52 @@ public class SearchElement {
 	private String progType;
 	private String progDayStart;
 	private String progDayEnd;
+	public SearchElement(String progNm, String placeDistrict, String progType, String progDayStart, String progDayEnd) {
+		super();
+		this.progNm = progNm;
+		this.placeDistrict = placeDistrict;
+		this.progType = progType;
+		this.progDayStart = progDayStart;
+		this.progDayEnd = progDayEnd;
+	}
+	public SearchElement() {
+		super();
+	}
+	public String getProgNm() {
+		return progNm;
+	}
+	public void setProgNm(String progNm) {
+		this.progNm = progNm;
+	}
+	public String getPlaceDistrict() {
+		return placeDistrict;
+	}
+	public void setPlaceDistrict(String placeDistrict) {
+		this.placeDistrict = placeDistrict;
+	}
+	public String getProgType() {
+		return progType;
+	}
+	public void setProgType(String progType) {
+		this.progType = progType;
+	}
+	public String getProgDayStart() {
+		return progDayStart;
+	}
+	public void setProgDayStart(String progDayStart) {
+		this.progDayStart = progDayStart;
+	}
+	public String getProgDayEnd() {
+		return progDayEnd;
+	}
+	public void setProgDayEnd(String progDayEnd) {
+		this.progDayEnd = progDayEnd;
+	}
+	
+	
+	
+	
+	
+	
 	
 }
