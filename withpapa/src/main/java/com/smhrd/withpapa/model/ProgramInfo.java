aeae4206faff069eb201507238c6997ca843cdae
smhrd <smhrd@192.168.219.45>
2024-06-17 09:15:14 +0900

    형동 - 매퍼 경로 수정, 쿼리 전달 시 카멜케이스에서 스네이크케이스 자동변환

---
+++ withpapa/src/main/java/com/smhrd/withpapa/model/ProgramInfo.java
@@ -0,0 +1,14 @@
+package com.smhrd.withpapa.model;
+
+public class ProgramInfo {
+
+	// 검색 결과를 보여주는 데 필요한 프로그램 정보들을 모은 클래스
+	// 이 클래스는 뒤에 만들 프로그램 상세 정보 클래스인
+	// ProgramInfoPlus의 상위 클래스가 된다 - 상속 사용할 예정
+	
+	
+	
+	private String progId;
+	
+	
+}
---
+++ withpapa/src/main/java/com/smhrd/withpapa/model/SearchElement.java
@@ -0,0 +1,20 @@
+package com.smhrd.withpapa.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class SearchElement {
+	
+	private String progNm;
+	private String placeDistrict;
+	private String progType;
+	private String progDayStart;
+	private String progDayEnd;
+	
+}
