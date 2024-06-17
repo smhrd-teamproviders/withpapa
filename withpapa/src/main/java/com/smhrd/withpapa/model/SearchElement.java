e043f1c16578d39cde6229efa95b1d25275523a7
김형동 <mwwicco9840@gmail.com>
2024-06-17 18:56:30 +0900

    형동 - 불필요한 lombok 어노테이션 및 임포트 삭제

--- withpapa/src/main/java/com/smhrd/withpapa/model/SearchElement.java
+++ withpapa/src/main/java/com/smhrd/withpapa/model/SearchElement.java
@@ -1,14 +1,5 @@
 package com.smhrd.withpapa.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
 public class SearchElement {
 	
 	private String progNm;
