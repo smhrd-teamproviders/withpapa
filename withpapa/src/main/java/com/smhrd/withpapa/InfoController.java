31b2478a49bbd645fa6fa0d560b7ffcea3d13467
김형동 <mwwicco9840@gmail.com>
2024-06-24 10:11:49 +0900

    형동 - view에 이미지 출력되도록 인코딩하는 기능 추가, 화면 간 연속적으로 이동되지 않은 문제 해결

--- withpapa/src/main/java/com/smhrd/withpapa/InfoController.java
+++ withpapa/src/main/java/com/smhrd/withpapa/InfoController.java
@@ -1,14 +1,10 @@
 package com.smhrd.withpapa;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -21,12 +17,11 @@ public class InfoController {
 
 	@Autowired
 	InfoService service;
-	
-	@RequestMapping(value="/info/programinfo")
-	public String programInfo(@RequestParam("progId")String progId,
-						      Model model) throws IOException {
+
+	@RequestMapping(value = "/search/programinfo")
+	public String programInfo(@RequestParam("progId") String progId, Model model) throws IOException {
 		ProgramInfo prog = service.programInfo(progId);
-		
+
 		String progReptDayStart = prog.getProgReptDayStart();
 		String progReptDayEnd = prog.getProgReptDayEnd();
 		int progReptTf = Integer.parseInt(prog.getProgReptTf());
@@ -36,101 +31,81 @@ public class InfoController {
 		String progTmEnd = prog.getProgTmEnd();
 		String progPeriod = "";
 		String progTime = "";
-		
+
 		// progReptTf - view에서 보여주는 값 변경
-		if(progReptTf ==1 ) {
+		if (progReptTf == 1) {
 			int viewMonthStart = Integer.parseInt(progReptDayStart.substring(0, 2));
-		    String viewDayStart = progReptDayStart.substring(2);
-		    int viewMonthEnd = Integer.parseInt(progReptDayEnd.substring(0, 2));
-		    String viewDayEnd = progReptDayEnd.substring(2);
-			if(progReptDayStart.equals("0101") && progReptDayEnd.equals("1231")) {
-				progPeriod="상시";
-			}else if(viewDayStart.equals("01") && Integer.parseInt(viewDayEnd) >=28) {
-				viewDayStart="월";
-				viewDayEnd="월";
+			String viewDayStart = progReptDayStart.substring(2);
+			int viewMonthEnd = Integer.parseInt(progReptDayEnd.substring(0, 2));
+			String viewDayEnd = progReptDayEnd.substring(2);
+			if (progReptDayStart.equals("0101") && progReptDayEnd.equals("1231")) {
+				progPeriod = "상시";
+			} else if (viewDayStart.equals("01") && Integer.parseInt(viewDayEnd) >= 28) {
+				viewDayStart = "월";
+				viewDayEnd = "월";
 				progPeriod = "매년 " + (viewMonthStart + viewDayStart) + " ~ " + (viewMonthEnd + viewDayEnd);
-			}else {
-				viewDayStart="월";
-				viewDayEnd="월";
+			} else {
+				viewDayStart = "월";
+				viewDayEnd = "월";
 				progPeriod = "매년 " + (viewMonthStart + viewDayStart) + " ~ " + (viewMonthEnd + viewDayEnd);
 			}
+		} else {
+			int viewYearDayStart = Integer.parseInt(progDayStart.substring(0, 4)); // 2024
+			int viewMonthDayStart = Integer.parseInt(progDayStart.substring(4, 6)); // 04
+			String viewDateStart = progDayStart.substring(6); // 06
+
+			int viewYearDayEnd = Integer.parseInt(progDayEnd.substring(0, 4)); // 2024
+			int viewMonthDayEnd = Integer.parseInt(progDayEnd.substring(4, 6)); // 10
+			String viewDateEnd = progDayEnd.substring(6); // 31
+			progPeriod = viewYearDayStart + "." + viewMonthDayStart + "." + viewDateStart + " ~ " + viewYearDayEnd + "."
+					+ viewMonthDayEnd + "." + viewDateEnd;
 		}
-		else {
-	      int viewYearDayStart = Integer.parseInt(progDayStart.substring(0, 4)); //2024
-	      int viewMonthDayStart = Integer.parseInt(progDayStart.substring(4, 6)); //04
-	      String viewDateStart = progDayStart.substring(6); //06
-	      
-	      int viewYearDayEnd = Integer.parseInt(progDayEnd.substring(0, 4)); //2024
-	      int viewMonthDayEnd = Integer.parseInt(progDayEnd.substring(4, 6)); //10
-	      String viewDateEnd = progDayEnd.substring(6); //31
-		  progPeriod = viewYearDayStart + "." + viewMonthDayStart + "." +  viewDateStart + " ~ " + viewYearDayEnd + "." + viewMonthDayEnd + "." + viewDateEnd;
-		}
-		
+
 		// progTmStart, progTmEnd - view에서 보여주는 값 변경
-		int viewHourTmStart=0;
-		int viewHourTmEnd =0;
-		String viewMinTmStart="";
-		String viewMinTmEnd ="";
-		
+		int viewHourTmStart = 0;
+		int viewHourTmEnd = 0;
+		String viewMinTmStart = "";
+		String viewMinTmEnd = "";
+
 		System.out.println(progTmStart != null);
-		if(!progTmStart.equals("")) {
-		 viewHourTmStart = Integer.parseInt(progTmStart.substring(0, 2));
-		 viewMinTmStart = progTmStart.substring(2);
+		if (!progTmStart.equals("")) {
+			viewHourTmStart = Integer.parseInt(progTmStart.substring(0, 2));
+			viewMinTmStart = progTmStart.substring(2);
 		}
-		if(!progTmEnd.equals("")) {
-		 viewHourTmEnd = Integer.parseInt(progTmEnd.substring(0, 2));
-		 viewMinTmEnd = progTmEnd.substring(2);
+		if (!progTmEnd.equals("")) {
+			viewHourTmEnd = Integer.parseInt(progTmEnd.substring(0, 2));
+			viewMinTmEnd = progTmEnd.substring(2);
 		}
-		
-		if(progTmStart.equals("") && progTmEnd.equals("")) {
+
+		if (progTmStart.equals("") && progTmEnd.equals("")) {
 			progTime = "-";
-		}else {
-	         String viewTmStart = viewHourTmStart + ":" + viewMinTmStart;
-	         String viewTmEnd = viewHourTmEnd + ":" + viewMinTmEnd;
-	         
-	         if(viewHourTmStart == 0) {
-	            viewTmStart = "";
-	         }
-	         if(viewHourTmEnd == 0) {
-	            viewTmEnd = "";
-	         }
-	         progTime = viewTmStart + " ~ " + viewTmEnd;
+		} else {
+			String viewTmStart = viewHourTmStart + ":" + viewMinTmStart;
+			String viewTmEnd = viewHourTmEnd + ":" + viewMinTmEnd;
+
+			if (viewHourTmStart == 0) {
+				viewTmStart = "";
+			}
+			if (viewHourTmEnd == 0) {
+				viewTmEnd = "";
+			}
+			progTime = viewTmStart + " ~ " + viewTmEnd;
 		}
 		
-		
-		String imgPathRoot = "C:\\Users\\smhrd\\git\\withpapa\\withpapa\\src\\main\\webapp\\resources\\img\\img_type\\";
-		
-		String imgProgType = prog.getProgType();
-		
-		String imgPath = imgPathRoot + imgProgType +".png";
-		File imgFile = new File(imgPath);
-		
+		// 프로그램 이미지 이름, 유형을 이미지로 변환
 		ImageToBase64 converter = new ImageToBase64();
-		String imgBase64String = converter.convert(imgFile);
-		
-		System.out.println(imgBase64String);
-		prog.setProgType(imgBase64String);
-		
-				
-		String progPathRoot = "C:\\Users\\smhrd\\git\\withpapa\\withpapa\\src\\main\\webapp\\resources\\img\\img_program\\";
-		
-		String progProgImg = prog.getImgNm();
-		
-		String progPath = progPathRoot + progProgImg +".png";
-		File progFile = new File(progPath);
-		
-		String imgBase64String2 = converter.convert(progFile);
-		
-		System.out.println(imgBase64String2);
-		prog.setImgNm(imgBase64String2);
-		
-				
+		String typePathRoot = "C:\\Users\\smhrd\\git\\withpapa\\withpapa\\src\\main\\webapp\\resources\\img\\img_type\\";
+		String imgPathRoot = "C:\\Users\\smhrd\\git\\withpapa\\withpapa\\src\\main\\webapp\\resources\\img\\img_program\\";
+
+		prog.setProgType(converter.encodeImg(typePathRoot, prog.getProgType(), "png"));
+		prog.setImgNm(converter.encodeImg(imgPathRoot, prog.getImgNm(), "png"));
+
 		model.addAttribute("progNm", prog.getProgNm());
 		model.addAttribute("progType", prog.getProgType());
 		model.addAttribute("progDayStart", prog.getProgDayStart());
 		model.addAttribute("progDayEnd", prog.getProgDayEnd());
 		model.addAttribute("progReptDayStart", prog.getProgReptDayStart());
-		model.addAttribute("progReptDayEnd", prog.getProgReptDayEnd());		
+		model.addAttribute("progReptDayEnd", prog.getProgReptDayEnd());
 		model.addAttribute("progReptTf", prog.getProgReptTf());
 		model.addAttribute("progReptDow", prog.getProgReptDow());
 		model.addAttribute("progCost", prog.getProgCost());
@@ -138,20 +113,19 @@ public class InfoController {
 		model.addAttribute("progContent", prog.getProgContent());
 		model.addAttribute("progTmStart", prog.getProgTmStart());
 		model.addAttribute("progTmEnd", prog.getProgTmEnd());
-		
+
 		model.addAttribute("placeNm", prog.getPlaceNm());
 		model.addAttribute("placeAddress", prog.getPlaceAddress());
 		model.addAttribute("placePhoneNo", prog.getPlacePhoneNo());
 		model.addAttribute("placeLatitd", prog.getPlaceLatitd());
 		model.addAttribute("placeLongtd", prog.getPlaceLongtd());
-		
+
 		model.addAttribute("imgNm", prog.getImgNm());
-			
+
 		model.addAttribute("progPeriod", progPeriod);
 		model.addAttribute("progTime", progTime);
 		return "programinfo";
-					
+
 	}
-	
-	
+
 }
--- withpapa/src/main/java/com/smhrd/withpapa/converter/ImageToBase64.java
+++ withpapa/src/main/java/com/smhrd/withpapa/converter/ImageToBase64.java
@@ -14,4 +14,21 @@ public class ImageToBase64 {
 		return Base64.getEncoder().encodeToString(fileContent);
 	}
 	
+	public String encodeImg(String imgPathRoot, String imgName, String imgExtension)
+			throws IOException {
+		/**
+		 * 이미지명과 유형 문자열을 이미지로 변경하는 메소드
+		 * 
+		 * @param imgPathRoot 이미지 파일이 있는 폴더 주소
+		 * @param imgName 이미지 파일의 이름
+		 * @param imgExtension 이미지 파일의 확장자명 ex) "png" "jpg" 등
+		 */
+		
+		String imgPath = imgPathRoot+imgName+"."+imgExtension;
+		File imgFile = new File(imgPath);
+		String imgBase64String = convert(imgFile);
+	
+		return imgBase64String;
+	}
+	
 }
