0fa593294b276babc180e8d37df96dddf0899ab4
곽세진 <rhkrtpwls85@gmail.com>
2024-07-31 15:14:24 +0900

    곽세진 - BackEnd : RecommendArrays (model) 생성자, 배열 선언

--- withpapa/src/main/java/com/smhrd/withpapa/model/RecommendArrays.java
+++ withpapa/src/main/java/com/smhrd/withpapa/model/RecommendArrays.java
@@ -1,5 +1,42 @@
 package com.smhrd.withpapa.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
 public class RecommendArrays {
-	
+
+	private String[] progRcmdSpring = { "PROG000003", "PROG000043", "PROG000072", "PROG000098", "PROG000115",
+			"PROG000126", "PROG000142", "PROG000166", "PROG000172", "PROG000178" };
+
+	private String[] progRcmdSummer = { "PROG000001", "PROG000017", "PROG000019", "PROG000039", "PROG000052",
+			"PROG000062", "PROG000086", "PROG000119", "PROG000135", "PROG000137" };
+
+	private String[] progRcmdAutumn = { "PROG000003", "PROG000008", "PROG000070", "PROG000090", "PROG000096",
+			"PROG000099", "PROG000118", "PROG000134", "PROG000167", "PROG000170" };
+
+	private String[] progRcmdWinter = { "PROG000010", "PROG000015", "PROG000045", "PROG000068", "PROG000091",
+			"PROG000101", "PROG000110", "PROG000128", "PROG000159", "PROG000176" };
+
+	public RecommendArrays() {
+		super();
+	}
+
+	public String[] getProgRcmdSpring() {
+		return progRcmdSpring;
+	}
+
+	public String[] getProgRcmdSummer() {
+		return progRcmdSummer;
+	}
+
+	public String[] getProgRcmdAutumn() {
+		return progRcmdAutumn;
+	}
+
+	public String[] getProgRcmdWinter() {
+		return progRcmdWinter;
+	}
+
 }
